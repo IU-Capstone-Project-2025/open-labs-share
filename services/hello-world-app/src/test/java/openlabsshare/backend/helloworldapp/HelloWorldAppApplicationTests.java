9d8677f15a2b3d22515afa079ccd646150777d7f
Mikhail <trifonov2812@gmail.com>
2025-06-11 13:15:59 +0300

    feat: add hello-world-app

---
+++ services/hello-world-app/src/test/java/openlabsshare/backend/helloworldapp/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package openlabsshare.backend.helloworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
