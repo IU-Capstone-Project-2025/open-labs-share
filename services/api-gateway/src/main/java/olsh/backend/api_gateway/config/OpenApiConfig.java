a647a99b205bb04498eba3f1c5e0a4dff591dd76
Nexonm <nikita96max@gmail.com>
2025-06-25 12:51:09 +0300

    fix(docs): delete redundant information in docs

--- services/api-gateway/src/main/java/olsh/backend/api_gateway/config/OpenApiConfig.java
+++ services/api-gateway/src/main/java/olsh/backend/api_gateway/config/OpenApiConfig.java
@@ -20,10 +20,6 @@ import org.springframework.context.annotation.Configuration;
         )
     ),
     servers = {
-        @Server(
-            description = "Local Environment",
-            url = "http://localhost:8080"
-        )
     }
 )
 @SecurityScheme(
