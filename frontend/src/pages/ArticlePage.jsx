7890d6a872dda23eed93b6bce8568f6745976026
Mikhail <trifonov2812@gmail.com>
2025-06-29 15:57:22 +0300

    fix(auth, users, frontend): update profile functionality connected with frontend and with new jwt for username update

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,8 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Document, Page } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
 
 import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
