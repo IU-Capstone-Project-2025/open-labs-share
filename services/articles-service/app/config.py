c8696359c4606b83b1ac637c05b8e7a223526aec
Timur Salakhov <claymix@yandex.ru>
2025-07-05 06:02:37 +0300

    feat(articles): changed .env and its example; established config logging; completed working dockerfile and docker-compose files; fixed download asset logging bug

--- services/articles-service/app/config.py
+++ services/articles-service/app/config.py
@@ -3,12 +3,20 @@ from dotenv import load_dotenv
 
 # Import built-in modules
 import os
+import logging
 
 # Check if there's .env file
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     # Load environment variables
     load_dotenv(dotenv_path)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
 class Config:
     # General config
@@ -19,8 +27,14 @@ class Config:
     MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
 
     # DB config
-    DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = os.getenv("DB_PORT", "5432")
-    DB_NAME = os.getenv("DB_NAME", "postgres")
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+    POSTGRES_NAME = os.getenv("POSTGRES_NAME", "postgres")
+
+for key, value in Config.__dict__.items():
+    if key.startswith("__"):
+        continue
+    logger.info(f"{key}: {value}")
+
