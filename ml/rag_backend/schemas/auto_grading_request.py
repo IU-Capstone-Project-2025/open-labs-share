030c0fd43d21160f38f49292c924feceecf3bc7a
|-y6o-| <mailkirill17@gmail.com>
2025-07-08 22:38:53 +0300

    feat(auto-grading): add celery messsage brocker to handle autograding tasks feat(auto-grading-backend): add webhook notification

--- ml/rag_backend/schemas/auto_grading_request.py
+++ ml/rag_backend/schemas/auto_grading_request.py
@@ -4,3 +4,5 @@ class AutoGradingRequest(BaseModel):
     uuid: str
     assignment_id: str
     submission_id: str
+    webhook_url: str 
+    # Example: "http://name-of-docker-service:internal-port-of-docker-service/"
